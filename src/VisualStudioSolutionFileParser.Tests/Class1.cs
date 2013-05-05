8f258fafc5a49e19bbbc018640d26c6c5893376c
David Alpert <david@spinthemoose.com>
2013-05-05 02:06:08 -0500

    filling out the grammer - can now parse basic project declarations and rough cuts of two more global sections

--- src/VisualStudioSolutionFileParser.Tests/Class1.cs
+++ src/VisualStudioSolutionFileParser.Tests/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using Sprache;
+using VisualStudioSolutionFileParser.Tests.Helpers;
 
 namespace VisualStudioSolutionFileParser.Tests
 {
@@ -58,7 +59,9 @@ namespace VisualStudioSolutionFileParser.Tests
         [Test]
         public void LoadSequence_is_a_recognized_SectionLoadSequence_value()
         {
+            Assert.AreEqual(SectionLoadSequence.Unrecognized, SolutionFileGrammar.LoadSequence.Parse("unknownValue"));
             Assert.AreEqual(SectionLoadSequence.PreSolution, SolutionFileGrammar.LoadSequence.Parse("preSolution"));
+            Assert.AreEqual(SectionLoadSequence.PostSolution, SolutionFileGrammar.LoadSequence.Parse("postSolution"));
         }
 
         [Test]
@@ -110,11 +113,81 @@ EndGlobalSection
             Assert.AreEqual(1, section.Properties.Count);
             Assert.AreEqual("SolutionProperties", section.Name);
         }
+
+        [Test]
+        public void BracketedGuid()
+        {
+            var input = "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}";
+
+            var result = SolutionFileGrammar.BracketedGuid.Parse(input);
+
+            Assert.AreEqual(new Guid(input), result);
+        }
+
+        [Test]
+        public void QuotedString()
+        {
+            var input = "\"serenity\"";
+
+            var result = SolutionFileGrammar.QuotedString.Parse(input);
+
+            Assert.AreEqual("serenity", result);
+        }
+
+        [Test]
+        [Ignore("Not sure what it is doing wrong...")]
+        public void Guid()
+        {
+            var input = "FAE04EC0-301F-11D3-BF4B-00C04F79EFBC";
+
+            var result = SolutionFileGrammar.Guid.Parse(input);
+
+            Assert.AreEqual(new Guid("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC"), result);
+        }
+
+        [Test]
+        public void QuotedGuid()
+        {
+            var input = "\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\"";
+
+            var result = SolutionFileGrammar.QuotedGuid.Parse(input);
+
+            Assert.AreEqual(new Guid("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC"), result);
+        }
+
+        [Test]
+        public void Project()
+        {
+            var input =
+@"Project(""{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}"") = ""HttpWebAdapters"", ""HttpWebAdapters\HttpWebAdapters.csproj"", ""{AE7D2A46-3F67-4986-B04B-7DCE79A549A5}""
+EndProject
+";
+            var result = SolutionFileGrammar.Project.Parse(input);
+
+            Assert.AreEqual(new Guid("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC"), result.ProjectType);
+            Assert.AreEqual(new Guid("AE7D2A46-3F67-4986-B04B-7DCE79A549A5"), result.ID);
+            Assert.AreEqual("HttpWebAdapters", result.Name);
+            Assert.AreEqual(@"HttpWebAdapters\HttpWebAdapters.csproj", result.Path);
+        }
+
+        [Test]
+        public void SolutionFile_can_have_projects()
+        {
+            var input = ReadSampleFile("Example.00.sln");
+
+            var result = SolutionFileGrammar.Solution.Parse(input);
+        }
+
+	    private string ReadSampleFile(string fileName)
+	    {
+	        string relativeManifestUri = String.Format("TestData.{0}", fileName);
+	        return ManifestResourceHelper.ExtractManifestResourceToString(relativeManifestUri);
+	    }
 	}
 
 	public class SolutionFileGrammar
 	{
-		#region stable
+	    #region stable
 
 		static readonly Parser<string> NewLine =
 			Parse.String(Environment.NewLine).Text();
@@ -143,23 +216,35 @@ EndGlobalSection
 			from name in ProductName
 			select new SolutionFileHeader(version, name);
 
-		#endregion
-
 	    public static readonly Parser<SectionLoadSequence> LoadSequence =
 	        from sequence in Parse.String("preSolution").Token().Return(SectionLoadSequence.PreSolution)
+                         .Or(Parse.String("postSolution").Token().Return(SectionLoadSequence.PostSolution))
+                         .Or(Parse.Return(SectionLoadSequence.Unrecognized))
 	        select sequence;
 
         public static readonly Parser<string> RoundBracketedString =
 			from left in Parse.Char('(').Token()
-			from content in Parse.CharExcept(')').Many().Text().Token()
+			from content in Parse.CharExcept(')').Many().Text()
 			from right in Parse.Char(')').Token()
 			select content;
 
-        public static readonly Parser<SolutionFileGlobalSection> UnrecognizedGlobalSection =
+        public static readonly Func<string,Parser<SolutionFileGlobalSection>> UnrecognizedGlobalSection = sectionName =>
             from ignore1 in Parse.Char('=').Token()
             from loadSequence in LoadSequence
-            from ignore2 in Parse.AnyChar.Until(Parse.String("EndGlobalSection").Token()).Text()
-            select new UnrecognizedGlobalSection(ignore2, loadSequence);
+            from content in Parse.AnyChar.Until(Parse.String("EndGlobalSection").Token()).Text()
+            select new UnrecognizedGlobalSection(sectionName, content, loadSequence);
+
+        public static readonly Parser<SolutionFileGlobalSection> SolutionConfigurationPlatformsGlobalSection = 
+            from ignore1 in Parse.Char('=').Token()
+            from loadSequence in LoadSequence
+            from content in Parse.AnyChar.Until(Parse.String("EndGlobalSection").Token()).Text()
+            select new SolutionConfigurationPlatformsGlobalSection(content, loadSequence);
+
+        public static readonly Parser<SolutionFileGlobalSection> ProjectConfigurationPlatformsGlobalSection =
+            from ignore1 in Parse.Char('=').Token()
+            from loadSequence in LoadSequence
+            from content in Parse.AnyChar.Until(Parse.String("EndGlobalSection").Token()).Text()
+            select new ProjectConfigurationPlatformsGlobalSection(content, loadSequence);
 
 	    public static readonly Parser<SolutionProperty> SolutionProperty =
 	        from name in Parse.CharExcept('=').Many().Text().Token()
@@ -181,7 +266,9 @@ EndGlobalSection
 	        from section in RoundBracketedString.Then(s =>
 	            {
 	                return s == "SolutionProperties" ? SolutionPropertiesGlobalSection
-	                                                 : UnrecognizedGlobalSection;
+                         : s == "SolutionConfigurationPlatforms" ? SolutionConfigurationPlatformsGlobalSection
+                         : s == "ProjectConfigurationPlatforms" ? ProjectConfigurationPlatformsGlobalSection
+	                                                 : UnrecognizedGlobalSection(s);
 	            })
             // end tag is swallowed by the specific section parsers
 	        select section;
@@ -192,11 +279,74 @@ EndGlobalSection
 	        from end in Parse.String("EndGlobal").Token()
 	        select sections;
 
+		#endregion
+
+	    public static readonly Parser<Guid> Guid =
+	        from part1 in Parse.LetterOrDigit.Repeat(8)
+	        from dash1 in Parse.Char('-')
+	        from part2 in Parse.LetterOrDigit.Repeat(4)
+	        from dash2 in Parse.Char('-')
+	        from part3 in Parse.LetterOrDigit.Repeat(4)
+	        from dash3 in Parse.Char('-')
+	        from part4 in Parse.LetterOrDigit.Repeat(4)
+	        from dash4 in Parse.Char('-')
+	        from part5 in Parse.LetterOrDigit.Repeat(13)
+            let guid = string.Format("{0}-{1}-{2}-{3}-{4}", part1, part2, part3, part4, part5)
+	        select System.Guid.Parse(guid);
+
+        public static readonly Parser<Guid> BracketedGuid =
+            (from leading in Parse.Char('{')
+             from value in Parse.CharExcept('}').Many().Text()
+             from trailing in Parse.Char('}')
+	         select new Guid(value)).Token();
+
+        public static readonly Parser<string> QuotedString =
+          (from leading in Parse.Char('"')
+           from content in Parse.CharExcept('"').Many().Text()
+           from trailing in Parse.Char('"')
+           select content).Token();
+
+        public static readonly Parser<Guid> QuotedGuid =
+            (from unquoted in QuotedString
+             select BracketedGuid.Parse(unquoted)).Token();
+
+	    public static readonly Parser<ProjectDeclaration> Project =
+            from start in Parse.String("Project")
+            from left in Parse.Char('(').Token()
+            from projectType in QuotedGuid
+            from right in Parse.Char(')').Token()
+            from ignore in Parse.Char('=').Token()
+            from projectName in QuotedString
+            from ignore2 in Parse.Char(',').Token()
+            from projectPath in QuotedString
+            from ignore3 in Parse.Char(',').Token()
+            from projectId in QuotedGuid
+            //from sections in SolutionFileProjectSection.Many().Optional()
+            from end in Parse.String("EndProject")
+            select new ProjectDeclaration(projectType, projectName, projectPath, projectId);
+
 	    public static readonly Parser<SolutionFile> Solution =
 	        from header in Header
+            from projects in Project.Many().Optional()
 	        from globals in Global
 	        select new SolutionFile(header, globals);
 	}
+
+    public class ProjectDeclaration
+    {
+        public ProjectDeclaration(Guid projectType, string projectName, string projectPath, Guid projectId)
+        {
+            ID = projectId;
+            Name = projectName;
+            ProjectType = projectType;
+            Path = projectPath;
+        }
+
+        public Guid ID { get; private set; }
+        public string Name { get; private set; }
+        public Guid ProjectType { get; private set; }
+        public string Path { get; private set; }
+    }
 
     public class SolutionProperty
     {
@@ -223,14 +373,33 @@ EndGlobalSection
 
     public class UnrecognizedGlobalSection : SolutionFileGlobalSection
     {
-        public UnrecognizedGlobalSection(string name, SectionLoadSequence loadSequence) : base(name, loadSequence)
+        public UnrecognizedGlobalSection(string name, string content, SectionLoadSequence loadSequence) : base(name, loadSequence)
+        {
+            throw new ParseException(string.Format("Unrecognized GlobalSection({0})", name));
+        }
+    }
+
+    public class SolutionConfigurationPlatformsGlobalSection : SolutionFileGlobalSection
+    {
+        public SolutionConfigurationPlatformsGlobalSection(string content, SectionLoadSequence loadSequence) 
+            : base(typeof(SolutionConfigurationPlatformsGlobalSection).Name, loadSequence)
+        {
+        }
+    }
+
+    public class ProjectConfigurationPlatformsGlobalSection : SolutionFileGlobalSection
+    {
+        public ProjectConfigurationPlatformsGlobalSection(string content, SectionLoadSequence loadSequence) 
+            : base(typeof(ProjectConfigurationPlatformsGlobalSection).Name, loadSequence)
         {
         }
     }
 
     public enum SectionLoadSequence
     {
-        PreSolution
+        Unrecognized,
+        PreSolution,
+        PostSolution
     }
 
 	public class SolutionFile
@@ -252,7 +421,10 @@ EndGlobalSection
 	public abstract class SolutionFileGlobalSection
 	{
 	    protected SolutionFileGlobalSection(string name, SectionLoadSequence loadSequence)
-		{
+	    {
+	        if (loadSequence == SectionLoadSequence.Unrecognized)
+	            throw new ParseException(String.Format("'{0}' has an unsupported load sequence.", name));
+
 			Name = name;
             LoadSequence = loadSequence;
 		}
