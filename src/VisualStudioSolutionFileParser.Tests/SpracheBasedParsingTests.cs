c7385d580c6e6a2b1a851e1892d342f6b07885cb
David Alpert <david@spinthemoose.com>
2014-01-26 12:03:30 -0600

    whitespace changes in old code

--- src/VisualStudioSolutionFileParser.Tests/SpracheBasedParsingTests.cs
+++ src/VisualStudioSolutionFileParser.Tests/SpracheBasedParsingTests.cs
@@ -2,57 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using Sprache;
 using VisualStudioSolutionFileParser.Tests.Helpers;
 
 namespace VisualStudioSolutionFileParser.Tests
 {
     [TestFixture]
-	public class SpracheBasedParsingTests
-	{
-		[Test]
-		public void Header_contains_version_information()
-		{
-			var input =
+    public class SpracheBasedParsingTests
+    {
+        [Test]
+        public void Header_contains_version_information()
+        {
+            var input =
 @"Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 2012";
 
-			var result = SolutionFileGrammar.Header.Parse(input);
+            var result = SolutionFileGrammar.Header.Parse(input);
 
-			Assert.AreEqual("Visual Studio 2012", result.ProductName);
-			Assert.AreEqual(12, result.MajorVersion);
-			Assert.AreEqual(00, result.MinorVersion);
-		}
+            Assert.AreEqual("Visual Studio 2012", result.ProductName);
+            Assert.AreEqual(12, result.MajorVersion);
+            Assert.AreEqual(00, result.MinorVersion);
+        }
 
-		[Test]
-		public void SolutionVersionNumber_is_Number_period_Number()
-		{
-			var input = @"12.00";
+        [Test]
+        public void SolutionVersionNumber_is_Number_period_Number()
+        {
+            var input = @"12.00";
 
-			var result = SolutionFileGrammar.SolutionVersionNumber.Parse(input);
+            var result = SolutionFileGrammar.SolutionVersionNumber.Parse(input);
 
-			Assert.AreEqual(12, result.Major);
-			Assert.AreEqual(00, result.Minor);
-		}
+            Assert.AreEqual(12, result.Major);
+            Assert.AreEqual(00, result.Minor);
+        }
 
-		[Test]
-		public void ProductName_is_pound_followed_by_text()
-		{
-			var input = @"# Visual Studio 2012";
+        [Test]
+        public void ProductName_is_pound_followed_by_text()
+        {
+            var input = @"# Visual Studio 2012";
 
-			var result = SolutionFileGrammar.ProductName.Parse(input);
+            var result = SolutionFileGrammar.ProductName.Parse(input);
 
-			Assert.AreEqual("Visual Studio 2012", result);
-		}
+            Assert.AreEqual("Visual Studio 2012", result);
+        }
 
-		[Test]
-		public void RoundBracketedString_is_string_surrounded_by_round_brackets()
-		{
-			var input = @"(something)";
+        [Test]
+        public void RoundBracketedString_is_string_surrounded_by_round_brackets()
+        {
+            var input = @"(something)";
 
-			var result = SolutionFileGrammar.RoundBracketedString.Parse(input);
+            var result = SolutionFileGrammar.RoundBracketedString.Parse(input);
 
-			Assert.AreEqual("something", result);
-		}
+            Assert.AreEqual("something", result);
+        }
 
         [Test]
         public void LoadSequence_is_a_recognized_SectionLoadSequence_value()
@@ -69,9 +71,9 @@ namespace VisualStudioSolutionFileParser.Tests
 @"Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 2012
 Global
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
+    GlobalSection(SolutionProperties) = preSolution
+        HideSolutionNode = FALSE
+    EndGlobalSection
 EndGlobal
 ";
             var result = SolutionFileGrammar.Solution.Parse(input);
@@ -98,7 +100,7 @@ EndGlobal
         {
             var input =
 @"GlobalSection(SolutionProperties) = preSolution
-	HideSolutionNode = FALSE
+    HideSolutionNode = FALSE
 EndGlobalSection
 ";
 
@@ -176,55 +178,55 @@ EndProject
             var result = SolutionFileGrammar.Solution.Parse(input);
         }
 
-	    private string ReadSampleFile(string fileName)
-	    {
-	        string relativeManifestUri = String.Format("TestData.{0}", fileName);
-	        return ManifestResourceHelper.ExtractManifestResourceToString(relativeManifestUri);
-	    }
-	}
+        private string ReadSampleFile(string fileName)
+        {
+            string relativeManifestUri = String.Format("TestData.{0}", fileName);
+            return ManifestResourceHelper.ExtractManifestResourceToString(relativeManifestUri);
+        }
+    }
 
-	public class SolutionFileGrammar
-	{
-	    #region stable
+    public class SolutionFileGrammar
+    {
+        #region stable
 
-		static readonly Parser<string> NewLine =
-			Parse.String(Environment.NewLine).Text();
+        static readonly Parser<string> NewLine =
+            Parse.String(Environment.NewLine).Text();
 
-		static readonly Parser<string> Eof =
-			Parse.Return("").End().XOr(
-			NewLine.End()).Or(
-			NewLine);
+        static readonly Parser<string> Eof =
+            Parse.Return("").End().XOr(
+            NewLine.End()).Or(
+            NewLine);
 
-		public static readonly Parser<SolutionVersionNumber> SolutionVersionNumber =
-			from rawMajor in Parse.Number.Token()
-			from period in Parse.Char('.')
-			from rawMinor in Parse.Number.Token()
-			let major = int.Parse(rawMajor)
-			let minor = int.Parse(rawMinor)
-			select new SolutionVersionNumber(major, minor);
+        public static readonly Parser<SolutionVersionNumber> SolutionVersionNumber =
+            from rawMajor in Parse.Number.Token()
+            from period in Parse.Char('.')
+            from rawMinor in Parse.Number.Token()
+            let major = int.Parse(rawMajor)
+            let minor = int.Parse(rawMinor)
+            select new SolutionVersionNumber(major, minor);
 
-		public static readonly Parser<string> ProductName =
-			from pound in Parse.Char('#').Token()
-			from name in Parse.AnyChar.Until(NewLine.Or(Eof)).Text()
-			select name;
+        public static readonly Parser<string> ProductName =
+            from pound in Parse.Char('#').Token()
+            from name in Parse.AnyChar.Until(NewLine.Or(Eof)).Text()
+            select name;
 
-		public static readonly Parser<SolutionFileHeader> Header =
-			from ignore1 in Parse.String("Microsoft Visual Studio Solution File, Format Version").Token()
-			from version in SolutionVersionNumber
-			from name in ProductName
-			select new SolutionFileHeader(version, name);
+        public static readonly Parser<SolutionFileHeader> Header =
+            from ignore1 in Parse.String("Microsoft Visual Studio Solution File, Format Version").Token()
+            from version in SolutionVersionNumber
+            from name in ProductName
+            select new SolutionFileHeader(version, name);
 
-	    public static readonly Parser<SectionLoadSequence> LoadSequence =
-	        from sequence in Parse.String("preSolution").Token().Return(SectionLoadSequence.PreSolution)
+        public static readonly Parser<SectionLoadSequence> LoadSequence =
+            from sequence in Parse.String("preSolution").Token().Return(SectionLoadSequence.PreSolution)
                          .Or(Parse.String("postSolution").Token().Return(SectionLoadSequence.PostSolution))
                          .Or(Parse.Return(SectionLoadSequence.Unrecognized))
-	        select sequence;
+            select sequence;
 
         public static readonly Parser<string> RoundBracketedString =
-			from left in Parse.Char('(').Token()
-			from content in Parse.CharExcept(')').Many().Text()
-			from right in Parse.Char(')').Token()
-			select content;
+            from left in Parse.Char('(').Token()
+            from content in Parse.CharExcept(')').Many().Text()
+            from right in Parse.Char(')').Token()
+            select content;
 
         public static readonly Func<string,Parser<SolutionFileGlobalSection>> UnrecognizedGlobalSection = sectionName =>
             from ignore1 in Parse.Char('=').Token()
@@ -244,13 +246,13 @@ EndProject
             from content in Parse.AnyChar.Until(Parse.String("EndGlobalSection").Token()).Text()
             select new ProjectConfigurationPlatformsGlobalSection(content, loadSequence);
 
-	    public static readonly Parser<SolutionProperty> SolutionProperty =
-	        from name in Parse.CharExcept('=').Many().Text().Token()
+        public static readonly Parser<SolutionProperty> SolutionProperty =
+            from name in Parse.CharExcept('=').Many().Text().Token()
             from ignore in Parse.Char('=').Token()
-	        from value in Parse.AnyChar.Until(NewLine.Or(Eof)).Text().Token()
+            from value in Parse.AnyChar.Until(NewLine.Or(Eof)).Text().Token()
             let sanitizedName = (name ?? string.Empty).Trim()
             let sanitizedValue = (value ?? string.Empty).Trim()
-	        select new SolutionProperty(sanitizedName, sanitizedValue);
+            select new SolutionProperty(sanitizedName, sanitizedValue);
 
         public static readonly Parser<SolutionFileGlobalSection> SolutionPropertiesGlobalSection =
             from ignore1 in Parse.Char('=').Token()
@@ -259,44 +261,44 @@ EndProject
             from end in Parse.AnyChar.Until(Parse.String("EndGlobalSection").Token()).Text()
             select new SolutionPropertiesSection(loadSequence, properties);
 
-	    public static readonly Parser<SolutionFileGlobalSection> GlobalSection =
-	        from start in Parse.String("GlobalSection").Token()
-	        from section in RoundBracketedString.Then(s =>
-	            {
-	                return s == "SolutionProperties" ? SolutionPropertiesGlobalSection
+        public static readonly Parser<SolutionFileGlobalSection> GlobalSection =
+            from start in Parse.String("GlobalSection").Token()
+            from section in RoundBracketedString.Then(s =>
+                {
+                    return s == "SolutionProperties" ? SolutionPropertiesGlobalSection
                          : s == "SolutionConfigurationPlatforms" ? SolutionConfigurationPlatformsGlobalSection
                          : s == "ProjectConfigurationPlatforms" ? ProjectConfigurationPlatformsGlobalSection
-	                                                 : UnrecognizedGlobalSection(s);
-	            })
+                                                     : UnrecognizedGlobalSection(s);
+                })
             // end tag is swallowed by the specific section parsers
-	        select section;
+            select section;
 
-	    public static readonly Parser<IEnumerable<SolutionFileGlobalSection>> Global =
-	        from start in Parse.String("Global").Token()
-	        from sections in GlobalSection.Many()
-	        from end in Parse.String("EndGlobal").Token()
-	        select sections;
+        public static readonly Parser<IEnumerable<SolutionFileGlobalSection>> Global =
+            from start in Parse.String("Global").Token()
+            from sections in GlobalSection.Many()
+            from end in Parse.String("EndGlobal").Token()
+            select sections;
 
-		#endregion
+        #endregion
 
-	    public static readonly Parser<Guid> Guid =
-	        from part1 in Parse.LetterOrDigit.Repeat(8)
-	        from dash1 in Parse.Char('-')
-	        from part2 in Parse.LetterOrDigit.Repeat(4)
-	        from dash2 in Parse.Char('-')
-	        from part3 in Parse.LetterOrDigit.Repeat(4)
-	        from dash3 in Parse.Char('-')
-	        from part4 in Parse.LetterOrDigit.Repeat(4)
-	        from dash4 in Parse.Char('-')
-	        from part5 in Parse.LetterOrDigit.Repeat(13)
+        public static readonly Parser<Guid> Guid =
+            from part1 in Parse.LetterOrDigit.Repeat(8)
+            from dash1 in Parse.Char('-')
+            from part2 in Parse.LetterOrDigit.Repeat(4)
+            from dash2 in Parse.Char('-')
+            from part3 in Parse.LetterOrDigit.Repeat(4)
+            from dash3 in Parse.Char('-')
+            from part4 in Parse.LetterOrDigit.Repeat(4)
+            from dash4 in Parse.Char('-')
+            from part5 in Parse.LetterOrDigit.Repeat(13)
             let guid = string.Format("{0}-{1}-{2}-{3}-{4}", part1, part2, part3, part4, part5)
-	        select System.Guid.Parse(guid);
+            select System.Guid.Parse(guid);
 
         public static readonly Parser<Guid> BracketedGuid =
             (from leading in Parse.Char('{')
              from value in Parse.CharExcept('}').Many().Text()
              from trailing in Parse.Char('}')
-	         select new Guid(value)).Token();
+             select new Guid(value)).Token();
 
         public static readonly Parser<string> QuotedString =
           (from leading in Parse.Char('"')
@@ -308,7 +310,7 @@ EndProject
             (from unquoted in QuotedString
              select BracketedGuid.Parse(unquoted)).Token();
 
-	    public static readonly Parser<ProjectDeclaration> Project =
+        public static readonly Parser<ProjectDeclaration> Project =
             from start in Parse.String("Project")
             from left in Parse.Char('(').Token()
             from projectType in QuotedGuid
@@ -323,12 +325,12 @@ EndProject
             from end in Parse.String("EndProject")
             select new ProjectDeclaration(projectType, projectName, projectPath, projectId);
 
-	    public static readonly Parser<SolutionFile> Solution =
-	        from header in Header
+        public static readonly Parser<SolutionFile> Solution =
+            from header in Header
             from projects in Project.Many().Optional()
-	        from globals in Global
-	        select new SolutionFile(header, globals);
-	}
+            from globals in Global
+            select new SolutionFile(header, globals);
+    }
 
     public class ProjectDeclaration
     {
@@ -400,60 +402,60 @@ EndProject
         PostSolution
     }
 
-	public class SolutionFile
-	{
-		public SolutionFile(SolutionFileHeader header, IEnumerable<SolutionFileGlobalSection> globals)
-		{
-		    MajorVersion = header.MajorVersion;
-		    MinorVersion = header.MinorVersion;
-		    ProductName = header.ProductName;
-		    GlobalSections = globals.ToDictionary(x => x.Name, x => x);
-		}
+    public class SolutionFile
+    {
+        public SolutionFile(SolutionFileHeader header, IEnumerable<SolutionFileGlobalSection> globals)
+        {
+            MajorVersion = header.MajorVersion;
+            MinorVersion = header.MinorVersion;
+            ProductName = header.ProductName;
+            GlobalSections = globals.ToDictionary(x => x.Name, x => x);
+        }
 
-		public int MajorVersion { get; private set; }
-		public int MinorVersion { get; private set; }
-		public string ProductName { get; private set; }
-		public Dictionary<string, SolutionFileGlobalSection> GlobalSections { get; private set; }
-	}
+        public int MajorVersion { get; private set; }
+        public int MinorVersion { get; private set; }
+        public string ProductName { get; private set; }
+        public Dictionary<string, SolutionFileGlobalSection> GlobalSections { get; private set; }
+    }
 
-	public abstract class SolutionFileGlobalSection
-	{
-	    protected SolutionFileGlobalSection(string name, SectionLoadSequence loadSequence)
-	    {
-	        if (loadSequence == SectionLoadSequence.Unrecognized)
-	            throw new ParseException(String.Format("'{0}' has an unsupported load sequence.", name));
+    public abstract class SolutionFileGlobalSection
+    {
+        protected SolutionFileGlobalSection(string name, SectionLoadSequence loadSequence)
+        {
+            if (loadSequence == SectionLoadSequence.Unrecognized)
+                throw new ParseException(String.Format("'{0}' has an unsupported load sequence.", name));
 
-			Name = name;
+            Name = name;
             LoadSequence = loadSequence;
-		}
+        }
 
-		public string Name { get; private set; }
-	    public SectionLoadSequence LoadSequence { get; private set; }
-	}
+        public string Name { get; private set; }
+        public SectionLoadSequence LoadSequence { get; private set; }
+    }
 
-	public class SolutionVersionNumber
-	{
-		public SolutionVersionNumber(int major, int minor)
-		{
-			Major = major;
-			Minor = minor;
-		}
+    public class SolutionVersionNumber
+    {
+        public SolutionVersionNumber(int major, int minor)
+        {
+            Major = major;
+            Minor = minor;
+        }
 
-		public int Major { get; private set; }
-		public int Minor { get; private set; }
-	}
+        public int Major { get; private set; }
+        public int Minor { get; private set; }
+    }
 
-	public class SolutionFileHeader
-	{
-		public SolutionFileHeader(SolutionVersionNumber version, string yearReleased)
-		{
-			MajorVersion = version.Major;
-			MinorVersion = version.Minor;
-			ProductName = yearReleased;
-		}
+    public class SolutionFileHeader
+    {
+        public SolutionFileHeader(SolutionVersionNumber version, string yearReleased)
+        {
+            MajorVersion = version.Major;
+            MinorVersion = version.Minor;
+            ProductName = yearReleased;
+        }
 
-		public int MajorVersion { get; private set; }
-		public int MinorVersion { get; private set; }
-		public string ProductName { get; private set; }
-	}
+        public int MajorVersion { get; private set; }
+        public int MinorVersion { get; private set; }
+        public string ProductName { get; private set; }
+    }
 }
